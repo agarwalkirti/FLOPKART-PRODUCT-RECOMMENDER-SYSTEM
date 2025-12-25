d2384531114cdacff5ddad003e0641d8d093835f
agarwalkirti <akirti302@gmail.com>
2025-12-26 02:13:50 +0530

    moving heavy initialization out of startup

--- app.py
+++ app.py
@@ -1,43 +1,46 @@
-from flask import render_template,Flask,request,Response
-from prometheus_client import Counter,generate_latest
-from flopkart.data_ingestion import DataIngestor
-from flopkart.rag_chain import RAGChainBuilder
-
+from flask import render_template, Flask, request, Response
+from prometheus_client import Counter, generate_latest
 from dotenv import load_dotenv
+
 load_dotenv()
 
-REQUEST_COUNT = Counter("http_requests_total" , "Total HTTP Request")
+REQUEST_COUNT = Counter("http_requests_total", "Total HTTP Request")
 
 def create_app():
-
     app = Flask(__name__)
 
-    vector_store = DataIngestor().ingest(load_existing=True)
-    rag_chain = RAGChainBuilder(vector_store).build_chain()
+    vector_store = None
+    rag_chain = None
+
+    @app.before_first_request
+    def init_rag():
+        nonlocal vector_store, rag_chain
+        from flopkart.data_ingestion import DataIngestor
+        from flopkart.rag_chain import RAGChainBuilder
+
+        vector_store = DataIngestor().ingest(load_existing=True)
+        rag_chain = RAGChainBuilder(vector_store).build_chain()
 
     @app.route("/")
     def index():
         REQUEST_COUNT.inc()
         return render_template("index.html")
-    
-    @app.route("/get" , methods=["POST"])
+
+    @app.route("/get", methods=["POST"])
     def get_response():
-
         user_input = request.form["msg"]
-
-        reponse = rag_chain.invoke(
-            {"input" : user_input},
-            config={"configurable" : {"session_id" : "user-session"}}
+        response = rag_chain.invoke(
+            {"input": user_input},
+            config={"configurable": {"session_id": "user-session"}}
         )["answer"]
+        return response
 
-        return reponse
-    
     @app.route("/metrics")
     def metrics():
         return Response(generate_latest(), mimetype="text/plain")
-    
+
     return app
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app = create_app()
-    app.run(host="0.0.0.0",port=5000,debug=True)
+    app.run(host="0.0.0.0", port=5000)
